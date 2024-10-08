77a9c7998765761beb20aee63d51540a695ca157
tataplayjiotv <saini000987098@gmail.com>
2024-10-08 10:42:43 +0530

    Update mpd.php

--- api/mpd.php
+++ api/mpd.php
@@ -5,7 +5,7 @@ $id = isset($_GET['id']) ? $_GET['id'] : '';
 // Check if id is not empty, then proceed with the redirection
 if (!empty($id)) {
     // Redirect to the new URL with the dynamic id
-    header("Location: https://babel-discord-playlist.vercel.app/cache/$id.mpd");
+    header("Location: https://babel-discord-playlist.vercel.app/cache/$id");
     exit;
 } else {
     // If no id is provided, you can handle the error or redirect to a default page
