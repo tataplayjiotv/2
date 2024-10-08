c04bd0d1c71da2a9b005004777847388b0fa56b7
tataplayjiotv <saini000987098@gmail.com>
2024-10-08 10:30:34 +0530

    Update key.php

--- api/key.php
+++ api/key.php
@@ -5,7 +5,7 @@ $id = isset($_GET['id']) ? $_GET['id'] : '';
 // Check if id is not empty, then proceed with the redirection
 if (!empty($id)) {
     // Redirect to the new URL with the dynamic id
-    header("Location: https://tplay.yuvraj49.in/tp/_____________/key.php?id=$id");
+    header("Location: https://babel-discord-playlist.vercel.app/tata/key/$id");
     exit;
 } else {
     // If no id is provided, you can handle the error or redirect to a default page
